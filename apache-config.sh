283ca2f5477e0d62f1f65507d1e1e5600f0178b1
Christian Dorner <christian.dorner@gmail.com>
2015-02-27 10:58:58 -0300

    Adding script to configure apache on local environment

---
+++ apache-config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+: ${SITES_AVAILABLE_HOME:=/etc/apache2/sites-available}
+: ${SITES_ENABLED_HOME:=/etc/apache2/sites-enabled}
+
+sudo rm -rf $SITES_ENABLED_HOME/vcerto
+sudo rm -rf $SITES_ENABLED_HOME/agende
+sudo rm -rf $SITES_ENABLED_HOME/agende-localhost
+
+sudo ln -s $SITES_AVAILABLE_HOME/agende-localhost $SITES_ENABLED_HOME/agende-localhost
+
+sudo service apache2 restart
